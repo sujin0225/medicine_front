820b08744f61ea54298f907d27b4aab2c9ad7b45
sujin0225 <suz225@naver.com>
2024-02-22 23:10:28 +0900

    기본정보, 전문가정보, 식별정보 데이터 추가

--- src/types/interface/medicine-list-item.interface.ts
+++ src/types/interface/medicine-list-item.interface.ts
@@ -7,4 +7,20 @@ export default interface MedicineListItem {
     FORM_CODE_NAME: string;
     ITEM_SEQ: string;
     ITEM_ENG_NAME: string;
+    CLASS_NO: string;
+    ITEM_PERMIT_DATE: string;
+    CHART: string;
+    EDI_CODE: string;
+    DRUG_SHAPE: string;
+    THICK: string;
+    LENG_LONG: string;
+    LENG_SHORT: string;
+    PRINT_FRONT: string;
+    PRINT_BACK: string;
+    IMG_REGIST_TS: string;
+    MARK_CODE_FRONT_ANAL: string;
+    MARK_CODE_BACK_ANAL: string;
+    MARK_CODE_FRONT_IMG: string;
+    MARK_CODE_BACK_IMG: string;
+    CHANGE_DATE: string;
 }
--- src/views/MedicineSearch/Detail/index.tsx
+++ src/views/MedicineSearch/Detail/index.tsx
@@ -50,16 +50,249 @@ export default function MedicineDetail() {
                   <button className={toggleState === 5 ? "tabs active-tabs" : "tabs"} onClick={() => toggleTab(5)}>리뷰</button>
               </div>
               <div className="content-tabs">
+                {/* 기본정보 tab */}
                   {toggleState === 1 && <div className="content active-content">
                     <div className='medicine-detail-tabs-box'>
-                      <div className='medicine-detail-basics'>성분명</div>
+                    {medicineList.map((item, index) => (
+                        <div className='medicine-detail-basics-content-box-main'>
+                        <div className='medicine-detail-basics-content-box' key={index}>
+                            <div className='medicine-detail-basics-title-top'>품목명</div>
+                            <div className='medicine-detail-text-container'>
+                        <div className='medicine-detail-basics-content-top'>{item.ITEM_NAME}</div>
+                    <div className='medicine-detail-basics-content-eng'>{item.ITEM_ENG_NAME}</div>
+                </div>
+            </div>
+            <div className='content-line-top'></div> 
+            <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>분류</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-two'>[{item.CLASS_NO}] {item.CLASS_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div> 
+        <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>구분</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-two'>{item.ETC_OTC_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div> 
+        <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>제형</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-two'>{item.FORM_CODE_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div> 
+        <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>보험적용</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.FORM_CODE_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div> 
+        <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>보험적용</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.FORM_CODE_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div> 
+        <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>투여경로</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.FORM_CODE_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div> 
+        <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>판매사명</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.ENTP_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div> 
+     </div>
+))}
+    </div>
+        </div>}
+        {/* 전문가정보 tab */}
+            {toggleState === 2 && <div className="content active-content">
+            <div className='medicine-detail-tabs-box'>
+            {medicineList.map((item, index) => (
+                <div className='medicine-detail-basics-content-box-main'>
+                    <div className='medicine-detail-basics-content-box' key={index}>
+                        <div className='medicine-detail-basics-title-top'>허가일</div>
+                        <div className='medicine-detail-text-container'>
+                        <div className='medicine-detail-basics-content-top'>{item.ITEM_PERMIT_DATE}</div>    
                     </div>
+                </div>
+            <div className='content-line-top'></div> 
+                <div className='medicine-detail-basics-content-box'>
+                    <div className='medicine-detail-basics-title'>표준코드(대표)</div>
+                    <div className='medicine-detail-text-container'>
+                {/* <div className='medicine-detail-basics-content'>[{item.CLASS_NO}] {item.CLASS_NAME}</div> */}
+            </div>
+        </div>
+        <div className='content-line'></div> 
+            <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>표준코드(일반)</div>
+                    <div className='medicine-detail-text-container'>
+                {/* <div className='medicine-detail-basics-content'>[{item.CLASS_NO}] {item.CLASS_NAME}</div> */}
+            </div>
+        </div>
+        <div className='content-line'></div>
+            <div className='medicine-detail-basics-content-box'>
+                <div className='medicine-detail-basics-title'>성상</div>
+                    <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'>{item.CHART}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>품목일련번호</div>
+                    <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'>{item.ITEM_SEQ}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+                    <div className='medicine-detail-basics-title'>ATC코드</div>
+                    <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'></div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+                    <div className='medicine-detail-basics-title'>주성분코드</div>
+                    <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'></div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+                    <div className='medicine-detail-basics-title'>보험코드</div>
+                    <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'>{item.EDI_CODE !== null && item.EDI_CODE !== "" ? item.EDI_CODE : "-"}</div>
+                
+            </div>
+        </div>
+        <div className='content-line'></div>
+                </div>
+            ))}
+            </div>
+                </div>}
+                  {toggleState === 3 && <div className="content active-content">
+                  <div className='medicine-detail-tabs-box-info'>
+                    {medicineList.map((item, index) => (
+                    <div className='medicine-detail-basics-content-box-main'>
+                        <div className='medicine-detail-basics-content-box' key={index}>
+                        <div className='medicine-detail-basics-title-top'>성상</div>
+                        <div className='medicine-detail-text-container'>
+                        <div className='medicine-detail-basics-content-info'>{item.CHART}</div>    
+                    </div>
+                </div>
+            <div className='content-line-top'></div> 
+            <div className='medicine-detail-basics-content-box'>
+                    <div className='medicine-detail-basics-title'>의약품모양</div>
+                    <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'>{item.DRUG_SHAPE}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>   
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>두께(mm)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'>{item.THICK}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>장축(mm)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'>{item.LENG_LONG}</div>
+            </div>
+        </div>        
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>단축(mm)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content'>{item.LENG_SHORT}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>표시(앞)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-two'>{item.PRINT_FRONT}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>표시(뒤)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-two'>{item.PRINT_BACK}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>약학정보원<br />이미지생성일</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.IMG_REGIST_TS}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>제형코드이름</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.FORM_CODE_NAME}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>마크내용(앞)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.MARK_CODE_FRONT_ANAL !== null && item.MARK_CODE_FRONT_ANAL !== "" ? item.MARK_CODE_FRONT_ANAL : "-"}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>마크내용(뒤)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.MARK_CODE_BACK_ANAL !== null && item.MARK_CODE_BACK_ANAL !== "" ? item.MARK_CODE_BACK_ANAL : "-"}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>마크이미지(앞)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.MARK_CODE_FRONT_IMG !== null && item.MARK_CODE_FRONT_IMG !== "" ? item.MARK_CODE_FRONT_IMG : "-"}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>마크이미지(뒤)</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.MARK_CODE_BACK_IMG !== null && item.MARK_CODE_BACK_IMG !== "" ? item.MARK_CODE_BACK_IMG : "-"}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+        <div className='medicine-detail-basics-content-box'>
+            <div className='medicine-detail-basics-title'>변경일자</div>
+                <div className='medicine-detail-text-container'>
+                <div className='medicine-detail-basics-content-four'>{item.CHANGE_DATE}</div>
+            </div>
+        </div>
+        <div className='content-line'></div>
+                </div>
+                
+                    ))}
+                  </div>
                     </div>}
-                  {toggleState === 2 && <div className="content active-content">2</div>}
-                  {toggleState === 3 && <div className="content active-content">3</div>}
                   {toggleState === 4 && <div className="content active-content">4</div>}
                   {toggleState === 5 && <div className="content active-content">5</div>}
               </div>
+              
           </div>
       </div>
   );
