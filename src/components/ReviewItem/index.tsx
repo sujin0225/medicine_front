346370e413c78cb414fb6c94c59d7bf728900d46
sujin0225 <suz225@naver.com>
2024-05-09 16:47:52 +0900

    별점 표시 이미지로 변경

--- src/components/ReviewItem/index.tsx
+++ src/components/ReviewItem/index.tsx
@@ -9,12 +9,25 @@ interface Props {
 export default function ReviewItem({reviewListItem}: Props) {
     const { reviewNumber, userId, itemSeq, content, writeDatetime, starRating, reviewImageList } = reviewListItem;
 
+    const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    if (i <= starRating) {
+      stars.push(<div className="star star-full" />); // full 별
+    } else {
+      stars.push(<div className="star star-empty" />); // empty 별
+    }
+  }
+
+
 return (
     <div className='review-list-item'>
         <div className='review-line'></div>
         <div className='review-list-item-top'>
             <div className='review-content-box'>
-                <div className='review-rating'>{starRating}점</div>
+                 <div className="review-rating">
+                    {stars}
+                    <div className='review-rating-number'>{starRating}</div>
+                    </div>
                 <div className='review-content-id'>{userId} | <div className='review-date'>{writeDatetime}</div></div>
                 <div className='review-content'>{content}</div>
                 {reviewImageList.map(image => <img className='review-image' src={image} />)}
